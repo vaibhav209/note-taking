d8062accde22b04080ff3a4677eb015fd520dfd6
vaibhav jumde <vaibhavjumde97@gmail.com>
2023-09-07 00:12:23 +0530

    chore: update dependencies

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <ToastContainer
+      position="top-center"
+      style={{ width: "400px" }}
+      autoClose={2000}
+    />
+    <App />
+  </BrowserRouter>
+);
